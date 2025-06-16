243b7c27800fd00da7880a7b3242c25899047764
Nomthandazoe <44575883+Nomthandazoe@users.noreply.github.com>
2025-06-16 22:55:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => res.send("Welcome"));
 app.use("/webhook", webhookRoute);
 
 app.listen(3000, () => console.log("Server is running on port 3000"));
